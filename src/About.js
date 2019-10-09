236166842c8abcc056ea397ecf7b65e3bcaa5a45
Yeri <arifyeripratama@gmail.com>
2019-10-09 17:46:53 +0700

    update about

--- src/About.js
+++ src/About.js
@@ -16,7 +16,7 @@ const About = () => {
             </div>
             <div className="col-xs-10 col-xs-offset-1 col-sm-offset-3 col-sm-6">
               <p className="teal-shadow">
-                I love to build stuff on the web, with over 4 years of
+                I love to build stuff on the web, with solid
                 experiences building multiple products for my companies where I
                 work, or as a freelancer.
               </p>
